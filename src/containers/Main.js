78d0a8dc15311624681518c7d8a2e3695b522c65
jack <jack@codeloom.co.uk>
2018-03-12 18:33:26 +0000

    not needed

--- src/containers/Main.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.scss';
-
-import {Route} from 'react-router-dom';
-
-ReactDOM.render(<App />, document.getElementById("app"));
